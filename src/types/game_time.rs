3656fe0a8e5d44c7caeacd2ecaf5787f1406c4ed
Roms1383 <romain.kelifa@gmail.com>
2024-06-22 17:40:13 +0700

    feat: add display and chrono support

--- src/types/game_time.rs
+++ src/types/game_time.rs
@@ -1,3 +1,5 @@
+use chrono::Timelike;
+
 use crate::raw::root::RED4ext as red;
 
 #[derive(Default, Clone, Copy)]
@@ -9,5 +11,20 @@ impl std::fmt::Display for GameTime {
     fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
         let [day, hour, min, sec] = unsafe { self.0.ToString() };
         write!(f, "{day}T{hour}:{min}:{sec}")
+    }
+}
+
+#[cfg(feature = "chrono")]
+impl From<GameTime> for chrono::DateTime<chrono::Utc> {
+    fn from(value: GameTime) -> Self {
+        // SAFETY: seconds being u32 it fits in i64, and nanos are zero
+        Self::from_timestamp(value.0.seconds as i64, 0).unwrap()
+    }
+}
+
+#[cfg(feature = "chrono")]
+impl From<chrono::DateTime<chrono::Utc>> for GameTime {
+    fn from(value: chrono::DateTime<chrono::Utc>) -> Self {
+        Self(red::GameTime { seconds: value.second() })
     }
 }
