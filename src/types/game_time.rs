6571af512cf55537c1c2e76add6d0e2d5f8c59c9
Roms1383 <romain.kelifa@gmail.com>
2024-06-22 18:18:44 +0700

    feat: impl time like

--- src/types/game_time.rs
+++ src/types/game_time.rs
@@ -1,5 +1,3 @@
-use chrono::Timelike;
-
 use crate::raw::root::RED4ext as red;
 
 #[derive(Default, Clone, Copy)]
@@ -25,8 +23,73 @@ impl From<GameTime> for chrono::DateTime<chrono::Utc> {
 #[cfg(feature = "chrono")]
 impl From<chrono::DateTime<chrono::Utc>> for GameTime {
     fn from(value: chrono::DateTime<chrono::Utc>) -> Self {
+        use chrono::Timelike;
         Self(red::GameTime {
             seconds: value.second(),
         })
+    }
+}
+
+#[cfg(feature = "chrono")]
+impl chrono::Timelike for GameTime {
+    fn hour(&self) -> u32 {
+        unsafe { self.0.GetHour() }
+    }
+
+    fn minute(&self) -> u32 {
+        unsafe { self.0.GetMinute() }
+    }
+
+    fn second(&self) -> u32 {
+        unsafe { self.0.GetSecond() }
+    }
+
+    fn nanosecond(&self) -> u32 {
+        0
+    }
+
+    fn with_hour(&self, hour: u32) -> Option<Self> {
+        if let Some(hour) = self.hour().checked_add(hour) {
+            let mut copy = *self;
+            unsafe { copy.0.AddHours(hour) };
+            return Some(copy);
+        }
+        None
+    }
+
+    fn with_minute(&self, min: u32) -> Option<Self> {
+        if let Some(min) = self.minute().checked_add(min) {
+            let mut copy = *self;
+            unsafe { copy.0.AddMinutes(min) };
+            return Some(copy);
+        }
+        None
+    }
+
+    fn with_second(&self, sec: u32) -> Option<Self> {
+        if let Some(sec) = self.second().checked_add(sec) {
+            let mut copy = *self;
+            unsafe { copy.0.AddSeconds(sec) };
+            return Some(copy);
+        }
+        None
+    }
+
+    /// GameTime does not support nanoseconds, so it will always return `None`
+    fn with_nanosecond(&self, _: u32) -> Option<Self> {
+        None
+    }
+
+    fn hour12(&self) -> (bool, u32) {
+        let hour = self.hour();
+        let mut hour12 = hour % 12;
+        if hour12 == 0 {
+            hour12 = 12;
+        }
+        (hour >= 12, hour12)
+    }
+
+    fn num_seconds_from_midnight(&self) -> u32 {
+        self.hour() * 3600 + self.minute() * 60 + self.second()
     }
 }
