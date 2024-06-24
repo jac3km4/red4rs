c75f04c65b7a6c241ef0871b7ea7e4e7714566cc
Roms1383 <romain.kelifa@gmail.com>
2024-06-24 15:54:44 +0700

    chore: add more definition to vft

--- src/systems/rtti.rs
+++ src/systems/rtti.rs
@@ -43,4 +43,47 @@ struct IRTTISystemVft {
         this: *const red::IRTTISystem,
         name: *const red::CName,
     ) -> *const red::CClass,
+    pub get_enum: unsafe extern "fastcall" fn(
+        this: *const red::IRTTISystem,
+        name: *const red::CName,
+    ) -> *const red::CEnum,
+    pub get_bit_field: unsafe extern "fastcall" fn(
+        this: *const red::IRTTISystem,
+        name: *const red::CName,
+    ) -> *const red::CBitfield,
+    sub_28: unsafe extern "fastcall" fn(this: *const red::IRTTISystem) -> VoidPtr,
+    pub get_function: unsafe extern "fastcall" fn(
+        this: *const red::IRTTISystem,
+        name: *const red::CName,
+    ) -> *const red::CBaseFunction,
+    sub_38: unsafe extern "fastcall" fn(this: *const red::IRTTISystem),
+    pub get_native_types: unsafe extern "fastcall" fn(
+        this: *const red::IRTTISystem,
+        out: *mut red::DynArray<*const red::CBaseRTTIType>,
+    ),
+    pub get_global_functions: unsafe extern "fastcall" fn(
+        this: *const red::IRTTISystem,
+        out: *mut red::DynArray<*const red::CBaseFunction>,
+    ),
+    sub_50: unsafe extern "fastcall" fn(this: *const red::IRTTISystem),
+    pub get_class_functions: unsafe extern "fastcall" fn(
+        this: *const red::IRTTISystem,
+        out: *mut red::DynArray<*const red::CBaseFunction>,
+    ),
+    pub get_enums: unsafe extern "fastcall" fn(
+        this: *const red::IRTTISystem,
+        out: *mut red::DynArray<*const red::CEnum>,
+    ),
+    pub get_bit_fields: unsafe extern "fastcall" fn(
+        this: *const red::IRTTISystem,
+        out: *mut red::DynArray<*const red::CBitfield>,
+        scripted_only: bool,
+    ),
+    pub get_classes: unsafe extern "fastcall" fn(
+        this: *const red::IRTTISystem,
+        class: *const red::CClass,
+        out: *mut red::DynArray<*const red::CClass>,
+        filter: *const fn(*const red::CClass) -> bool,
+        include_abstract: bool,
+    ),
 }
