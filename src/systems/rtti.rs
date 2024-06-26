b3f85ab69786337e73b9fcf66f25f189be1531e5
Roms1383 <romain.kelifa@gmail.com>
2024-06-26 17:50:30 +0700

    chore: other attempt

--- src/systems/rtti.rs
+++ src/systems/rtti.rs
@@ -5,37 +5,41 @@ use crate::{Array, Bitfield, CName, Class, Enum, Function, Type};
 pub struct RttiSystem(red::CRTTISystem);
 
 impl RttiSystem {
-    pub fn get() -> Self {
-        Self(unsafe { std::ptr::read(red::CRTTISystem_Get()) })
+    pub fn get() -> &'static Self {
+        unsafe {
+            std::mem::transmute::<&'static red::CRTTISystem, &'static Self>(
+                &*(red::CRTTISystem_Get() as *const _) as &'static red::CRTTISystem,
+            )
+        }
     }
 
     #[inline]
     pub fn get_class(&self, name: CName) -> Option<&Class> {
-        let ty = unsafe { (self.vft().get_class)(self as *const _ as *mut _, name) };
+        let ty = unsafe { (self.vft().get_class)(self, name) };
         unsafe { ty.cast::<Class>().as_ref() }
     }
 
     #[inline]
     pub fn get_type(&self, name: CName) -> Option<&Type> {
-        let ty = unsafe { (self.vft().get_type)(self as *const _ as *mut _, name) };
+        let ty = unsafe { (self.vft().get_type)(self, name) };
         unsafe { ty.cast::<Type>().as_ref() }
     }
 
     #[inline]
     pub fn get_enum(&self, name: CName) -> Option<&Enum> {
-        let ty = unsafe { (self.vft().get_enum)(self as *const _ as *mut _, name) };
+        let ty = unsafe { (self.vft().get_enum)(self, name) };
         unsafe { ty.cast::<Enum>().as_ref() }
     }
 
     #[inline]
     pub fn get_bitfield(&self, name: CName) -> Option<&Bitfield> {
-        let ty = unsafe { (self.vft().get_bitfield)(self as *const _ as *mut _, name) };
+        let ty = unsafe { (self.vft().get_bitfield)(self, name) };
         unsafe { ty.cast::<Bitfield>().as_ref() }
     }
 
     #[inline]
     pub fn get_function(&self, name: CName) -> Option<&Function> {
-        let ty = unsafe { (self.vft().get_function)(self as *const _ as *mut _, name) };
+        let ty = unsafe { (self.vft().get_function)(self, name) };
         unsafe { ty.cast::<Function>().as_ref() }
     }
 
@@ -44,7 +48,7 @@ impl RttiSystem {
         let mut out = Array::default();
         unsafe {
             (self.vft().get_native_types)(
-                self as *const _ as *mut _,
+                self,
                 &mut out as *mut Array<Type> as *mut Array<*const Type>,
             )
         };
@@ -56,7 +60,7 @@ impl RttiSystem {
         let mut out = Array::default();
         unsafe {
             (self.vft().get_enums)(
-                self as *const _ as *mut _,
+                self,
                 &mut out as *mut Array<Enum> as *mut Array<*const Enum>,
             )
         };
@@ -68,7 +72,7 @@ impl RttiSystem {
         let mut out = Array::default();
         unsafe {
             (self.vft().get_bitfields)(
-                self as *const _ as *mut _,
+                self,
                 &mut out as *mut Array<Bitfield> as *mut Array<*const Bitfield>,
                 scripted_only,
             )
@@ -81,7 +85,7 @@ impl RttiSystem {
         let mut out = Array::default();
         unsafe {
             (self.vft().get_global_functions)(
-                self as *const _ as *mut _,
+                self,
                 &mut out as *mut Array<Function> as *mut Array<*const Function>,
             )
         };
@@ -93,7 +97,7 @@ impl RttiSystem {
         let mut out = Array::default();
         unsafe {
             (self.vft().get_class_functions)(
-                self as *const _ as *mut _,
+                self,
                 &mut out as *mut Array<Function> as *mut Array<*const Function>,
             )
         };
@@ -106,7 +110,7 @@ impl RttiSystem {
         let mut out = Array::default();
         unsafe {
             (self.vft().get_classes)(
-                self as *const _ as *mut _,
+                self,
                 base,
                 &mut out as *mut Array<Class> as *mut Array<*const Class>,
                 None,
@@ -122,7 +126,7 @@ impl RttiSystem {
         let mut out = Array::default();
         unsafe {
             (self.vft().get_derived_classes)(
-                self as *const _ as *mut _,
+                self,
                 base,
                 &mut out as *mut Array<Class> as *mut Array<*const Class>,
             )
@@ -132,13 +136,13 @@ impl RttiSystem {
 
     #[inline]
     pub fn get_class_by_script_name(&self, name: CName) -> Option<&Class> {
-        let ty = unsafe { (self.vft().get_class_by_script_name)(self as *const _ as *mut _, name) };
+        let ty = unsafe { (self.vft().get_class_by_script_name)(self, name) };
         unsafe { ty.cast::<Class>().as_ref() }
     }
 
     #[inline]
     pub fn get_enum_by_script_name(&self, name: CName) -> Option<&Enum> {
-        let ty = unsafe { (self.vft().get_enum_by_script_name)(self as *const _ as *mut _, name) };
+        let ty = unsafe { (self.vft().get_enum_by_script_name)(self, name) };
         unsafe { ty.cast::<Enum>().as_ref() }
     }
 
@@ -150,64 +154,68 @@ impl RttiSystem {
 
 #[repr(C)]
 struct IRTTISystemVft {
-    get_type: unsafe extern "fastcall" fn(this: *mut RttiSystem, name: CName) -> *const Type,
+    get_type: unsafe extern "fastcall" fn(this: *const RttiSystem, name: CName) -> *const Type,
     get_type_by_async_id:
-        unsafe extern "fastcall" fn(this: *mut RttiSystem, async_id: u32) -> *const Type,
-    get_class: unsafe extern "fastcall" fn(this: *mut RttiSystem, name: CName) -> *const Class,
-    get_enum: unsafe extern "fastcall" fn(this: *mut RttiSystem, name: CName) -> *const Enum,
+        unsafe extern "fastcall" fn(this: *const RttiSystem, async_id: u32) -> *const Type,
+    get_class: unsafe extern "fastcall" fn(this: *const RttiSystem, name: CName) -> *const Class,
+    get_enum: unsafe extern "fastcall" fn(this: *const RttiSystem, name: CName) -> *const Enum,
     get_bitfield:
-        unsafe extern "fastcall" fn(this: *mut RttiSystem, name: CName) -> *const Bitfield,
-    _sub_28: unsafe extern "fastcall" fn(this: *mut RttiSystem),
+        unsafe extern "fastcall" fn(this: *const RttiSystem, name: CName) -> *const Bitfield,
+    _sub_28: unsafe extern "fastcall" fn(this: *const RttiSystem),
     get_function:
-        unsafe extern "fastcall" fn(this: *mut RttiSystem, name: CName) -> *const Function,
-    _sub_38: unsafe extern "fastcall" fn(this: *mut RttiSystem),
+        unsafe extern "fastcall" fn(this: *const RttiSystem, name: CName) -> *const Function,
+    _sub_38: unsafe extern "fastcall" fn(this: *const RttiSystem),
     get_native_types:
-        unsafe extern "fastcall" fn(this: *mut RttiSystem, out: *mut Array<*const Type>),
+        unsafe extern "fastcall" fn(this: *const RttiSystem, out: *mut Array<*const Type>),
     get_global_functions:
-        unsafe extern "fastcall" fn(this: *mut RttiSystem, out: *mut Array<*const Function>),
-    _sub_50: unsafe extern "fastcall" fn(this: *mut RttiSystem),
+        unsafe extern "fastcall" fn(this: *const RttiSystem, out: *mut Array<*const Function>),
+    _sub_50: unsafe extern "fastcall" fn(this: *const RttiSystem),
     get_class_functions:
-        unsafe extern "fastcall" fn(this: *mut RttiSystem, out: *mut Array<*const Function>),
-    get_enums: unsafe extern "fastcall" fn(this: *mut RttiSystem, out: *mut Array<*const Enum>),
+        unsafe extern "fastcall" fn(this: *const RttiSystem, out: *mut Array<*const Function>),
+    get_enums: unsafe extern "fastcall" fn(this: *const RttiSystem, out: *mut Array<*const Enum>),
     get_bitfields: unsafe extern "fastcall" fn(
-        this: *mut RttiSystem,
+        this: *const RttiSystem,
         out: *mut Array<*const Bitfield>,
         scripted_only: bool,
     ),
     get_classes: unsafe extern "fastcall" fn(
-        this: *mut RttiSystem,
+        this: *const RttiSystem,
         base_class: *const Class,
         out: *mut Array<*const Class>,
         filter: Option<unsafe extern "C" fn(*const Class) -> bool>,
         include_abstract: bool,
     ),
     get_derived_classes: unsafe extern "fastcall" fn(
-        this: *mut RttiSystem,
+        this: *const RttiSystem,
         base_class: *const Class,
         out: *mut Array<*const Class>,
     ),
     register_type:
-        unsafe extern "fastcall" fn(this: *mut RttiSystem, ty: *const Type, async_id: u32),
-    _sub_88: unsafe extern "fastcall" fn(this: *mut RttiSystem),
-    _sub_90: unsafe extern "fastcall" fn(this: *mut RttiSystem),
+        unsafe extern "fastcall" fn(this: *const RttiSystem, ty: *const Type, async_id: u32),
+    _sub_88: unsafe extern "fastcall" fn(this: *const RttiSystem),
+    _sub_90: unsafe extern "fastcall" fn(this: *const RttiSystem),
     unregister_type:
-        unsafe extern "fastcall" fn(this: *mut RttiSystem, ty: *const red::CBaseRTTIType),
+        unsafe extern "fastcall" fn(this: *const RttiSystem, ty: *const red::CBaseRTTIType),
     register_function:
-        unsafe extern "fastcall" fn(this: *mut RttiSystem, function: *const red::CGlobalFunction),
+        unsafe extern "fastcall" fn(this: *const RttiSystem, function: *const red::CGlobalFunction),
     unregister_function:
-        unsafe extern "fastcall" fn(this: *mut RttiSystem, function: *const red::CGlobalFunction),
-    _sub_b0: unsafe extern "fastcall" fn(this: *mut RttiSystem),
-    _sub_b8: unsafe extern "fastcall" fn(this: *mut RttiSystem),
+        unsafe extern "fastcall" fn(this: *const RttiSystem, function: *const red::CGlobalFunction),
+    _sub_b0: unsafe extern "fastcall" fn(this: *const RttiSystem),
+    _sub_b8: unsafe extern "fastcall" fn(this: *const RttiSystem),
     // FIXME: crashes when used, signature is probably wrong
-    _add_register_callback:
-        unsafe extern "fastcall" fn(this: *mut RttiSystem, function: unsafe extern "C" fn() -> ()),
+    _add_register_callback: unsafe extern "fastcall" fn(
+        this: *const RttiSystem,
+        function: unsafe extern "C" fn() -> (),
+    ),
     // FIXME: crashes when used, signature is probably wrong
-    _add_post_register_callback:
-        unsafe extern "fastcall" fn(this: *mut RttiSystem, function: unsafe extern "C" fn() -> ()),
-    _sub_d0: unsafe extern "fastcall" fn(this: *mut RttiSystem),
-    _sub_d8: unsafe extern "fastcall" fn(this: *mut RttiSystem),
+    _add_post_register_callback: unsafe extern "fastcall" fn(
+        this: *const RttiSystem,
+        function: unsafe extern "C" fn() -> (),
+    ),
+    _sub_d0: unsafe extern "fastcall" fn(this: *const RttiSystem),
+    _sub_d8: unsafe extern "fastcall" fn(this: *const RttiSystem),
     _create_scripted_class: unsafe extern "fastcall" fn(
-        this: *mut RttiSystem,
+        this: *const RttiSystem,
         name: CName,
         flags: red::CClass_Flags,
         parent: *const Class,
@@ -215,7 +223,7 @@ struct IRTTISystemVft {
     // FIXME: signature is wrong, but how to represent name and value of enumerator ?
     // https://github.com/WopsS/RED4ext.SDK/blob/124984353556f7b343041b810040062fbaa96196/include/RED4ext/RTTISystem.hpp#L50
     _create_scripted_enum: unsafe extern "fastcall" fn(
-        this: *mut RttiSystem,
+        this: *const RttiSystem,
         name: CName,
         size: i8,
         variants: *mut Array<u64>,
@@ -223,20 +231,23 @@ struct IRTTISystemVft {
     // FIXME: signature is wrong, but how to represent name and bit ?
     // https://github.com/WopsS/RED4ext.SDK/blob/124984353556f7b343041b810040062fbaa96196/include/RED4ext/RTTISystem.hpp#L54
     _create_scripted_bitfield:
-        unsafe extern "fastcall" fn(this: *mut RttiSystem, name: CName, bits: *mut Array<u64>),
-    _initialize_script_runtime: unsafe extern "fastcall" fn(this: *mut RttiSystem),
-    register_script_name:
-        unsafe extern "fastcall" fn(this: *mut RttiSystem, native_name: CName, script_name: CName),
+        unsafe extern "fastcall" fn(this: *const RttiSystem, name: CName, bits: *mut Array<u64>),
+    _initialize_script_runtime: unsafe extern "fastcall" fn(this: *const RttiSystem),
+    register_script_name: unsafe extern "fastcall" fn(
+        this: *const RttiSystem,
+        native_name: CName,
+        script_name: CName,
+    ),
     get_class_by_script_name:
-        unsafe extern "fastcall" fn(this: *mut RttiSystem, name: CName) -> *const Class,
+        unsafe extern "fastcall" fn(this: *const RttiSystem, name: CName) -> *const Class,
     get_enum_by_script_name:
-        unsafe extern "fastcall" fn(this: *mut RttiSystem, name: CName) -> *const Enum,
+        unsafe extern "fastcall" fn(this: *const RttiSystem, name: CName) -> *const Enum,
     // FIXME: crashes when used, signature is probably wrong
     _convert_native_to_script_name:
-        unsafe extern "fastcall" fn(this: *mut RttiSystem, name: red::CName) -> red::CName,
+        unsafe extern "fastcall" fn(this: *const RttiSystem, name: red::CName) -> red::CName,
     // FIXME: crashes when used, signature is probably wrong
     _convert_script_to_native_name:
-        unsafe extern "fastcall" fn(this: *mut RttiSystem, name: red::CName) -> red::CName,
+        unsafe extern "fastcall" fn(this: *const RttiSystem, name: red::CName) -> red::CName,
 }
 
 pub struct RTTIRegistrator;
