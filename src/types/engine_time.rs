1702742e42607a190e826338e4042abdcd2414db
Rom's <romain.kelifa@gmail.com>
2024-07-01 09:47:36 +0700

    docs: safety note
    Co-authored-by: jekky <gh@jekky.dev>

--- src/types/engine_time.rs
+++ src/types/engine_time.rs
@@ -15,7 +15,9 @@ impl EngineTime {
         f64::from_ne_bytes(self.0.unk00)
     }
 
-    /// SAFETY: panics if the sum ends up being `f64::NAN`, `f64::INFINITY` or `f64::NEG_INFINITY`.
+    /// # Panics
+    ///
+    /// Panics if the sum ends up being `f64::NAN`, `f64::INFINITY` or `f64::NEG_INFINITY`.
     pub fn saturating_add_assign(&mut self, value: impl Into<f64>) {
         let current = self.as_secs_f64();
         let value: f64 = value.into();
