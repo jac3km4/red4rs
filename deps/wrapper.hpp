2ba12556a26a9c23f5302aa3adaafc08a66b26d3
Roms1383 <romain.kelifa@gmail.com>
2024-07-09 20:00:47 +0700

    chore: generate vector4

--- deps/wrapper.hpp
+++ deps/wrapper.hpp
@@ -1,4 +1,5 @@
 #include <RED4ext/RED4ext.hpp>
+#include <RED4ext/Scripting/Natives/Generated/Vector4.hpp>
 #include <RED4ext/Scripting/Natives/entEntityID.hpp>
 #include <RED4ext/Scripting/Natives/GameTime.hpp>
 #include <RED4ext/Scripting/Natives/Generated/EngineTime.hpp>
